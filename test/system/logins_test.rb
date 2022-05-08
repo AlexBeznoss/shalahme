f2f2a2a638d2ea8419bded535cad1b6ab17bee3f
Alex Beznos <alex.b@humanagency.com>
2022-05-08 18:18:26 +0300

    Add test for login

---
+++ test/system/logins_test.rb
@@ -0,0 +1,35 @@
+require 'application_system_test_case'
+
+class LoginsTest < ApplicationSystemTestCase
+  test 'login by google' do
+    info = Faker::Omniauth.google
+    User.create!(uid: info[:uid], provider: info[:provider])
+    OmniAuth.config.add_mock(:google_oauth2, info)
+
+    visit root_path
+
+    assert_selector 'a', text: 'Sign in with Google'
+
+    assert_no_changes -> { User.count } do
+      click_on 'Sign in with Google'
+    end
+
+    assert_selector 'a', text: 'Dashboard'
+  end
+
+  test 'creates user by github' do
+    info = Faker::Omniauth.github
+    User.create!(uid: info[:uid], provider: info[:provider])
+    OmniAuth.config.add_mock(:github, info)
+
+    visit root_path
+
+    assert_selector 'a', text: 'Sign in with Github'
+
+    assert_no_changes -> { User.count } do
+      click_on 'Sign in with Github'
+    end
+
+    assert_selector 'a', text: 'Dashboard'
+  end
+end
