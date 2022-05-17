180cd9ad326bde94e2b22557b2f5b2116e9ed455
Egor <egor.slam@gmail.com>
2022-05-17 10:02:46 +0200

    Add logic for users update

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  before_action :require_signin
+  before_action :find_user, only: %i[edit update]
+
+  def index
+    @users = User.all
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash.now[:info] = 'User successfully updated!'
+
+      respond_to do |f|
+        f.turbo_stream
+        f.html { redirect_to phones_path }
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+end
