426397ec87e8a227d844b7ef74841a82491d0e98
Egor <egor.slam@gmail.com>
2022-05-25 16:23:26 +0200

    Fix rubocop

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module Shalahme
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.exceptions_app = self.routes
+    config.exceptions_app = routes
   end
 end
